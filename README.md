fee15d6a9b3210e977d5ed82ce61cc5e07fa9bb1
TheTiGuR <thetigur@gmail.com>
2021-09-06 22:58:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-hmxtbz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hmxtbz)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+import './sample';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+bob();
